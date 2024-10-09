4cae72a1c6800b333d59a4f5d93fea7df3cfe5f9
zerobug110 <mahabojang110>
2024-10-09 17:35:13 +0000

    router setup and config

--- admin/src/main.tsx
+++ admin/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import AppRoutes from "./components/routes/routes.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const router = createBrowserRouter(AppRoutes);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    {/* @ts-ignore */}
+      <RouterProvider router={router}>
+        <App />
+      </RouterProvider>
+  </React.StrictMode>
+);
