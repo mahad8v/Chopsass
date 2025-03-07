ab3f3bbe5f7d393606f5451de593a978ad721d2d
MahaD8v <mahabojang110@gmail.com>
2025-03-07 13:18:21 +0000

    test changes

--- admin/src/components/app-page-wrapper/PageWrapper.tsx
+++ admin/src/components/app-page-wrapper/PageWrapper.tsx
@@ -4,10 +4,7 @@ interface PageWrapperProps {
   children: ReactNode;
 }
 
+//*** note
 export const PageWrapper = ({ children }: PageWrapperProps) => {
-  return (
-    <div className='ml-72 mt-20 min-h-[100vh]'>
-      {children}
-    </div>
-  );
+  return <div className="ml-72 mt-20 min-h-[100vh]">{children}</div>;
 };
